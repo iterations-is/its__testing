f9b9ef01a601fad1fa7f8249f8ef889d6c4cc79f
Sergey Dunaevskiy <sergey@dunaevskiy.dev>
2022-04-05 14:47:52 +0200

    add: auth reset password

---
+++ test/auth-reset-password.test.js
@@ -0,0 +1,16 @@
+const { client } = require('../utils/axios');
+const { v4: uuid } = require('uuid');
+
+describe('auth - password reset', () => {
+	it('should receive successful password reset message (valid email)', async () => {
+		const email = "admin@example.com";
+		const res = await client.post('/auth-service/reset-password', { email });
+		expect(res.status).toBe(200);
+	});
+
+	it('should receive successful password reset message (invalid email)', async () => {
+		const email = `${uuid().split('').reverse().join('').slice(0, 12)}@iterations.com`;
+		const res = await client.post('/auth-service/reset-password', { email });
+		expect(res.status).toBe(200);
+	});
+});
